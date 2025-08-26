93bbfb60d9c6573016e28b89e90d4535ccd27e22
thomasdj17 <thomasdj17@yahoo.com>
2025-08-26 11:22:11 -0500

    small project looping and removing 	new file:   shopping_cart.py

---
+++ hello.py
@@ -0,0 +1,14 @@
+print("Hello, Day 3!")
+
+foods = (['eggs', 'bacon', 'grits'])
+for food in foods :
+    print(food)
+
+numbers = ([1, 2, 3, 4, 5])
+for evenNumbers in numbers:
+    print(evenNumbers)
+
+cart = ["apples", "bananas", "milk", "bread"]
+cart.remove("bread")
+for item in cart:
+    print("I need to buy", item)
